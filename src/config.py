b14ba223b68b5750026ea26538ffbc9ed5d8875c
Vladislav Veluga <vladislav805@yandex.com>
2020-07-01 00:31:20 +0300

    Add check for existing files and isdir

--- src/config.py
+++ src/config.py
@@ -2,9 +2,10 @@ import subprocess
 import re
 import sys
 
+from plugins.pattern import Pattern
 from plugins.plugin import Plugin
 from plugins.speed import Speed
-from utils import parse_time
+from utils import parse_time, check_file, Exist, print_error, print_confirm
 
 
 class Config:
@@ -16,6 +17,30 @@ class Config:
     def __init__(self, input_file: str, output_file: str):
         self.input_filename = input_file
         self.output_filename = output_file
+
+        self._check_files()
+
+    def _check_files(self):
+        input_status = check_file(self.input_filename)
+
+        is_pattern = self.get_plugin(Pattern)
+
+        if is_pattern is None:
+            if input_status == Exist.NOT_EXISTS:
+                print_error('input file "{0}" not exists'.format(self.input_filename))
+            if input_status == Exist.DIR:
+                print_error('input file "{0}" is directory'.format(self.input_filename))
+
+        output_status = check_file(self.output_filename)
+
+        if output_status is Exist.DIR:
+            print_error('output file "{0}" is directory'.format(self.output_filename))
+
+        if output_status is Exist.EXISTS:
+            ans = print_confirm('output file "{0}" already exists. Overwrite?'.format(self.output_filename))
+            if ans is False:
+                print('Cancelled')
+                exit(0)
 
     def add_plugin(self, plugin: Plugin):
         self.plugins.append(plugin)
@@ -87,9 +112,9 @@ class Config:
                     self._duration = parse_time(match[0])
 
             if self._frames is None:
-                match = re.search(r', (\d+) tbr', line, flags=re.IGNORECASE)
+                match = re.search(r', ([\d.]+) tbr', line, flags=re.IGNORECASE)
                 if match:
-                    self._frames = int(match[1])
+                    self._frames = float(match[1])
 
             if line.startswith('frame'):
                 status = re.search(
@@ -98,10 +123,10 @@ class Config:
                     flags=re.IGNORECASE
                 )
 
-                frame = int(status[1])
-                size = status[2]
+                frame = int(status[1].strip())
+                size = status[2].strip()
                 time = parse_time(status[3])
-                speed = float(status[4])
+                speed = float(status[4].strip())
 
                 sys.stdout.write('Processing... {0}%, speed {1}x\r'.format(
                     self._get_percent(frame, size, time, speed),
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,11 @@
+from os.path import isdir, exists
+from enum import Enum
+from colorama import Fore, Back, Style
+
 import re
+import colorama
+
+colorama.init()
 
 
 def parse_int(s) -> int:
@@ -21,3 +28,28 @@ def parse_time(time) -> int:
     time = hours * 3600 + minutes * 60 + seconds
 
     return time if mills == 0 else time + (mills / (10 ** len(res[5])))
+
+
+class Exist(Enum):
+    NOT_EXISTS = 0
+    EXISTS = 1
+    DIR = 2
+
+
+def check_file(path) -> Exist:
+    if exists(path):
+        return Exist.DIR if isdir(path) else Exist.EXISTS
+    return Exist.NOT_EXISTS
+
+
+def print_error(message: str, exit_code: int = 1) -> None:
+    print('{0}Error: {1}{2}'.format(Fore.RED, message, Style.RESET_ALL))
+    exit(exit_code)
+
+
+def print_confirm(message) -> bool:
+    ans = None
+    while ans not in ('y', 'n'):
+        ans = input('{0}{1}{2}'.format(Fore.YELLOW, message, Style.RESET_ALL))
+
+    return ans == 'y'
